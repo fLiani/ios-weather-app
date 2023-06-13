761cccf0f8adf0ae2d57d8b3afca791ddb16bf06
Fabrizio Liani <f.liani@lancaster.ac.uk>
2023-06-13 15:29:48 +0100

    Initial commit

---
+++ ios-weather-app/ios-weather-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios-weather-app
+//
+//  Created by Fabrizio Liani on 13/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-weather-app/ios-weather-app/ios_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_weather_appApp.swift
+//  ios-weather-app
+//
+//  Created by Fabrizio Liani on 13/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
