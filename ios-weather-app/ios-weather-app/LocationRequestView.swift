cd8db4d7a0e61ccfcc73bb6a725423f4c881b440
Fabrizio Liani <f.liani@lancaster.ac.uk>
2023-07-09 23:51:00 +0100

    Created location manager file, working on requesting location

---
+++ ios-weather-app/ios-weather-app/LocationRequestView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationRequestView.swift
+//  ios-weather-app
+//
+//  Created by Fabrizio Liani on 09/07/2023.
+//
+
+import SwiftUI
+
+struct LocationRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationRequestView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationRequestView()
+    }
+}
